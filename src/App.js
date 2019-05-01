ace7f219a727ef029153cd269edc5a903f6c1c28
Todd Tate <toddtatedev@gmail.com>
2019-04-30 18:04:50 -0600

    App.js
    Imported the Game Component into App.js in src

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
-import React, { Component } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Header from "./components/Header/Header";
-import Wrapper from "./components/Wrapper/Wrapper";
-import Character from "./components/Character/Character";
-import characters from "../src/characters.json";
-import './App.css';
+import React from "react";
+import Game from "./components/Game";
 
-class App extends Component {
-  state = {
-    characters
-  };
-
-
-
-  render() {
-
-    return (
-      <>
-      <Navbar>
-      </Navbar>
-      <Header>
-      </Header>
-      <Wrapper>
-        {this.state.characters.map(character => (
-          <Character 
-          key={character.id}
-          image={character.image}
-          />
-          ))}
-      </Wrapper>
-    </>
-  )
-}
+function App() {
+  return <Game />
 }
 
 export default App;
