da68462ab4fcf3c94f28406fe07d2f2298763c06
Todd Tate <toddtatedev@gmail.com>
2019-05-06 09:49:10 -0600

    Updates

---
+++ precache-manifest.89e89891d70bb128bc0877dc7a8c60a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cc04a7efee86e3af75656e3aee355d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7dd7630ed96e7e013f1f",
+    "url": "/clicky-game/static/css/main.09bd050f.chunk.css"
+  },
+  {
+    "revision": "7810a584dcb45b76e1a9",
+    "url": "/clicky-game/static/js/2.c037bd84.chunk.js"
+  },
+  {
+    "revision": "7dd7630ed96e7e013f1f",
+    "url": "/clicky-game/static/js/main.78521572.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
